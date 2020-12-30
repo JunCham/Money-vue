0e7d6bf0b00c849eb2988e8b596cf19108d149a6
JunCham <573373910@qq.com>
2020-12-30 17:33:37 +0800

    添加SCSS样式

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,13 @@
 
 <style lang="scss">
   body{
+    line-height: 1.5;
     margin:0;
     box-sizing: border-box;
+  }
+  a{
+    text-decoration: none;
+    color: inherit;
   }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
