fb2f2e5f461abc5487c02f21b2337201b54e96ea
JunCham <573373910@qq.com>
2021-01-12 18:10:04 +0800

    HTML写好了+模块化

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    <Layout>Money.vue</Layout>
+    <Layout class-prefix="layout">
+      <NumberPad/>
+      <Types/>
+      <Notes/>
+      <Tags/>
+    </Layout>
   </div>
 </template>
 
 <script lang="ts">
+import NumberPad from '@/components/Money/NumberPad.js';
+import Types from '@/components/Money/Types.js';
+import Notes from '@/components/Money/Notes.js';
+import Tags from '@/components/Money/Tags.js';
 export default {
   name: "Money",
+  components: {Tags, Notes, Types, NumberPad},
 }
 </script>
+<style lang="scss">
+  .layout-content{
+    display: flex;
+    flex-direction: column-reverse;
+  }
+</style>
