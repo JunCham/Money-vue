bdfee65f7c756bc156140d505831578056a80e8d
JunCham <573373910@qq.com>
2021-01-25 17:05:45 +0800

    ID生成器

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -1,6 +1,7 @@
 let id: number = parseInt(window.localStorage.getItem('_id_max') || '0') || 0;
 function createId(){
-    id++
+    id++；
+    window.localStorage.setItem('_idMax_',id.toString());
     return id
 }
 export default createId;
