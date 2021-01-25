589958f53a0de1b623e0b85be76a472cfaf3aa92
JunCham <573373910@qq.com>
2021-01-25 17:02:56 +0800

    ID生成器

---
+++ src/lib/createId.ts
@@ -0,0 +1,6 @@
+let id: number = parseInt(window.localStorage.getItem('_id_max') || '0') || 0;
+function createId(){
+    id++
+    return id
+}
+export default createId;
--- src/models/tagsListModel.ts
+++ src/models/tagsListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/lib/createId';
+
 const localStorageKeyName = 'tagsList';
 type Tag = {
     id: string;
@@ -21,7 +23,8 @@ const tagsListModel: TagsListModel = {
     create(name) {
         const names = this.data.map(item => item.name);
         if (names.indexOf(name) >= 0){ return 'duplicated'};
-        this.data.push({id: name,name: name});
+        const id = createId().toString();
+        this.data.push({id, name: name});
         this.save();
         return 'success';
     },
