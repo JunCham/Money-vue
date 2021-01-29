21a05d68b3482e4d3d15bb85f49117e04db67c17
JunCham <573373910@qq.com>
2021-01-29 11:44:02 +0800

    模块化Store ,删除了 models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,32 +1,8 @@
-import recordListModel from '../models/recordListModel';
-import tagsListModel from '../models/tagsListModel';
+import tagStore from '@/store/tagStore';
+import recordStore from '@/store/recordStore';
 
 const store = {
-//record store
-    recordList: recordListModel.fetch(),
-    createRecord: (record:RecordItem) => recordListModel.create(record),
-// 全局变量太多了
-//tag store
-    tagList: tagsListModel.fetch(),
-    findTag(id: string) {
-        return this.tagList.filter(t => t.id === id)[0];
-    },
-    createTag: (name :string) =>{
-        const message = tagsListModel.create(name);
-        if (name){
-            const message = tagsListModel.create(name);
-            if (message === 'duplicated'){
-                window.alert('标签名重复');
-            }else if (message === 'success'){
-                window.alert('添加成功');
-            }
-        }
-    },
-    removeTag: (id: string) => {
-        return tagsListModel.remove(id);
-    },
-    updateTag: (id: string,name: string) =>{
-        return tagsListModel.update(id,name)
-    },
+    ...tagStore,
+    ...recordStore,
 };
 export default store;
