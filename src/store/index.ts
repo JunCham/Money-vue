c25d7371f7d3f6ac89d297357bdceeb0b65f15a5
JunCham <573373910@qq.com>
2021-02-21 16:22:21 +0800

    EditLabel.vue 使用了 Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,12 +5,21 @@ import createId from '@/lib/createId';
 
 Vue.use(Vuex);  //吧store绑定到vue.prototype
 
+type RootState = {
+  recordList: RecordItem[],
+  tagList: Tag[],
+  currentTag?: Tag
+}
 const store = new Vuex.Store({
   state: {  //data
-    recordList: [] as RecordItem[],
-    tagList: [] as Tag[],
-  },
+    recordList: [],
+    tagList: [],
+    currentTag: undefined,
+  } as RootState,
   mutations: { // methods
+    setCurrentTag(state, id: string){
+      state.currentTag = state.tagList.filter(t => t.id === id)[0];
+    },
     fetchRecords(state){
       state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
     },
@@ -32,13 +41,11 @@ const store = new Vuex.Store({
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0){
         window.alert('标签名重复');
-        return 'duplicated'
       };
       const id = createId().toString();
       state.tagList.push({id, name: name});
       store.commit('saveTags');
       window.alert('添加成功');
-      return 'success';
     },
     saveTags(state){
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
